117f8e741cbc6630f50983e3c00b61be7a23693c
Duncan <th710703@gmail.com>
2019-01-13 23:44:28 +0800

    OnePair

--- src/Suit.php
+++ src/Suit.php
@@ -21,6 +21,7 @@ class Suit
         'straight',
         'threeOfAKind',
         'twoPairs',
+        'onePair',
     ];
     private $typeGroup;
     private $numberGroup;
@@ -155,6 +156,17 @@ class Suit
     {
         if (count($this->numberGroup) == 3 && max($this->numberGroup) == 2 && min($this->numberGroup) == 1) {
             $this->cardType = 'TwoPairs';
+            $this->cardPoint = array_merge(
+                array_keys($this->numberGroup, max($this->numberGroup)),
+                array_keys($this->numberGroup, min($this->numberGroup))
+            );
+        }
+    }
+
+    private function onePair()
+    {
+        if (count($this->numberGroup) == 4 && max($this->numberGroup) == 2) {
+            $this->cardType = 'OnePair';
             $this->cardPoint = array_merge(
                 array_keys($this->numberGroup, max($this->numberGroup)),
                 array_keys($this->numberGroup, min($this->numberGroup))
