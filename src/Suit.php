1c2c69699c4e84ac7c15b5633552bbdbeb0b4cb8
Duncan <th710703@gmail.com>
2019-01-13 23:40:02 +0800

    TwoParis

--- src/Suit.php
+++ src/Suit.php
@@ -20,6 +20,7 @@ class Suit
         'flush',
         'straight',
         'threeOfAKind',
+        'twoPairs',
     ];
     private $typeGroup;
     private $numberGroup;
@@ -148,5 +149,16 @@ class Suit
         }
 
         return false;
+    }
+
+    private function twoPairs()
+    {
+        if (count($this->numberGroup) == 3 && max($this->numberGroup) == 2 && min($this->numberGroup) == 1) {
+            $this->cardType = 'TwoPairs';
+            $this->cardPoint = array_merge(
+                array_keys($this->numberGroup, max($this->numberGroup)),
+                array_keys($this->numberGroup, min($this->numberGroup))
+            );
+        }
     }
 }
